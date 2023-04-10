a1f1c737d5208f0dc251a6836901b5f89a76d18c
shun-harutaro <shun_harutaro@protonmail.com>
2023-04-10 10:13:40 +0900

    remove next transpile

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: [
-    '@fullcalendar/react',
-    '@fullcalendar/common',
-    '@fullcalendar/daygrid',
-  ],
 }
 
 module.exports = nextConfig
