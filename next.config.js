446a526e3b35f8d276765d5f0c1a3412928ded3d
shun-harutaro <shun_harutaro@protonmail.com>
2023-04-10 10:09:21 +0900

    fix calendar's css

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    '@fullcalendar/react',
+    '@fullcalendar/common',
+    '@fullcalendar/daygrid',
+  ],
 }
 
 module.exports = nextConfig
