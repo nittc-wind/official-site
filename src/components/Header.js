d6d79ffbefb64c76fb4bf35c31fce2b56385645e
shun-harutaro <shun_harutaro@protonmail.com>
2023-04-18 19:31:32 +0900

    header items are centered

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,20 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="">
-      <h1>
+    <header className="m-2">
+      <h1 className='inline-block'>
         <Link href="/">
           <img
                 className='sm:max-w-md'
                 src="/icon.color.svg" alt='豊田高専吹奏楽部' />
         </Link>
       </h1>
-      <nav className='flex justify-between'>
+      <nav className='mt-2 flex justify-center'>
         <ul className='flex space-x-4'>
-           <li><Link href="/about">部活紹介</Link></li> 
-           <li><Link href="/about">年間予定</Link></li>
-           <li><Link href="/about">ニュース</Link></li>
-           <li><Link href="/about">コンタクト</Link></li>
+           <li className='hover:underline'><Link href="/about">部活紹介</Link></li> 
+           <li className='hover:underline'><Link href="/about">年間予定</Link></li>
+           <li className='hover:underline'><Link href="/about">ニュース</Link></li>
+           <li className='hover:underline'><Link href="/about">コンタクト</Link></li>
         </ul>
       </nav>
     </header>
