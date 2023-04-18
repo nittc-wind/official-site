a8966e40d3228de6a19be8bc9d1e1c468fcc822d
shun-harutaro <shun_harutaro@protonmail.com>
2023-04-18 20:26:15 +0900

    add calendar btn

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import Item from '../components/Item';
 import Calendar from '../components/Calendar';
 
 const Index = ({ article }) => {
+  const urlGoogleCal = "https://calendar.google.com/calendar/embed?src=d94109cf325d4d56e437d3e81e5ce206311f552bf1967da2ccf92653897e6c29%40group.calendar.google.com&ctz=Asia%2FTokyo";
+  const urlICal = "https://calendar.google.com/calendar/ical/d94109cf325d4d56e437d3e81e5ce206311f552bf1967da2ccf92653897e6c29%40group.calendar.google.com/public/basic.ics";
   return (
       <Layout>
         <Seo pageTitle="豊田高専吹奏楽部" />
@@ -21,6 +23,14 @@ const Index = ({ article }) => {
         </div>
         <h2 className='text-xl font-bold my-5'>練習予定</h2>
         <Calendar />
+        <p>部員向け</p>
+        <Link 
+          className='underline text-blue-600'
+          href={urlGoogleCal}>Googleカレンダーに追加</Link>
+        <span> / </span>
+        <Link 
+          className='underline text-blue-600'
+          href={urlICal}>iOS標準カレンダーはこちら</Link>
       </Layout>
   )
 }
