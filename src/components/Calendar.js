d96c6dde3414cf19e537e124191bd0d06fcf54fa
NOZAKI Shuntaro <60352276+shun-harutaro@users.noreply.github.com>
2024-11-03 00:25:11 +0900

    fullcalendarに戻した (#60)

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,17 +1,30 @@
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import listPlugin from '@fullcalendar/list'
+import googleCalendarPlugin from '@fullcalendar/google-calendar'
+
 export default function Calendar() {
   return (
-    <div style={{ position: "relative", paddingBottom: "75%", height: 0 }}>
-      <iframe
-        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FTokyo&showPrint=0&showTz=0&hl=ja&mode=AGENDA&title&showTitle=0&src=ZDk0MTA5Y2YzMjVkNGQ1NmU0MzdkM2U4MWU1Y2UyMDYzMTFmNTUyYmYxOTY3ZGEyY2NmOTI2NTM4OTdlNmMyOUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23EF6C00"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          border: "none",
+    <FullCalendar
+        plugins={[ dayGridPlugin, googleCalendarPlugin, listPlugin]}
+        locale="ja"
+        initialView="listWeek"
+        googleCalendarApiKey={process.env.NEXT_PUBLIC_CAL_API_KEY}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,listWeek'
         }}
-      ></iframe>
-    </div>
+        buttonText={{
+          today: "今日",
+          month: "月",
+          list: "週"
+        }}
+        eventSources={[
+        {
+          googleCalendarId: process.env.NEXT_PUBLIC_CAL_ID,
+        }
+      ]}
+    />
   )
 }
